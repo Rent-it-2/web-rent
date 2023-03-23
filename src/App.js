f04ca1d51ddd67598884d1fa05d89e9d0ed3fbf0
icarodante <icaro.menezes@sptech.school>
2023-03-22 21:29:05 -0300

    reorganização do projeto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import SearchBanner from "./componentes/search-banner/SearchBanner"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBanner/>
+    </div>
+  );
+}
+
+export default App;
